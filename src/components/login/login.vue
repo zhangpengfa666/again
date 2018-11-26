fab65992dcda7a7f84707bc261ba74e21488776f
Zpf <347820927@qq.com>
2018-11-26 18:06:04 +0800

    'login的引入和样式调整'

--- src/components/login/login.vue
+++ src/components/login/login.vue
@@ -1,13 +1,46 @@
 <template>
-    <div>login组件</div>
+<div class="login-wrap">
+    <el-form label-position="top" label-width="80px" :model="formdata" class="login-form">
+        <h2>用户登录</h2>
+        <el-form-item label="用户名">
+            <el-input v-model="formdata.username"></el-input>
+        </el-form-item>
+        <el-form-item label="密码">
+            <el-input v-model="formdata.password"></el-input>
+        </el-form-item>
+        <el-button class="login-button" type="primary">登录</el-button>
+    </el-form>
+</div>
 </template>
 
 <script>
 export default {
-
+    data () {
+        return {
+            formdata:{
+                username:'',
+                password:''
+            }
+        }
+    }
 }
 </script>
 
 <style>
-
+.login-wrap{
+    background-color: #324152;
+    height: 100%;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+}
+.login-wrap .login-form{
+    background-color: #fff;
+    padding: 30px;
+    width: 400px;   
+    border-radius: 5px;
+}
+.login-form .login-button{
+    width: 100%;
+}
 </style>
--- src/main.js
+++ src/main.js
@@ -4,6 +4,7 @@ import Vue from 'vue'
 import ElementUI from 'element-ui'
 import 'element-ui/lib/theme-chalk/index.css'
 import App from './App'
+import '@/assets/css/index.css'
 
 import router from './router'
 Vue.use(ElementUI)
