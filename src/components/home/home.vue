82386c0a5978eab4e2ec94c928677b6f3679f399
Zpf <347820927@qq.com>
2018-11-26 20:39:48 +0800

    'home组件侧边栏及退出功能完成'

--- src/components/home/home.vue
+++ src/components/home/home.vue
@@ -14,13 +14,84 @@
             </el-col>
             <el-col :span="1">
                 <div class="grid-content bg-purple">
-                    <a href="#" class="loginout">退出</a>
+                    <a href="#" class="loginout" @click.prevent="loginout()">退出</a>
                 </div>
             </el-col>
         </el-row>
     </el-header>
     <el-container>
-        <el-aside width="200px" class="aside">Aside</el-aside>
+        <el-aside width="200px" class="aside">
+            <el-menu 
+                class="el-menu-vertical-demo"
+                :unique-opened="true" 
+            >
+                <el-submenu index="1">
+                    <template slot="title">
+                        <i class="el-icon-location"></i>
+                        <span>用户管理</span>
+                    </template>
+            
+                        <el-menu-item index="1-1">
+                            <i class="el-icon-location"></i>用户列表
+                        </el-menu-item>
+                </el-submenu>
+                <!-- 2 -->
+                <el-submenu index="2">
+                    <template slot="title">
+                        <i class="el-icon-location"></i>
+                        <span>权限管理</span>
+                    </template>
+            
+                        <el-menu-item index="1-1">
+                            <i class="el-icon-location"></i>角色列表
+                        </el-menu-item>
+
+                        <el-menu-item index="1-1">
+                            <i class="el-icon-location"></i>权限列表
+                        </el-menu-item>
+                </el-submenu>
+                <!-- 3 -->
+                <el-submenu index="3">
+                    <template slot="title">
+                        <i class="el-icon-location"></i>
+                        <span>商品管理</span>
+                    </template>
+            
+                        <el-menu-item index="1-1">
+                            <i class="el-icon-location"></i>商品列表
+                        </el-menu-item>
+                        <el-menu-item index="1-1">
+                            <i class="el-icon-location"></i>分类参数
+                        </el-menu-item>
+                        <el-menu-item index="1-1">
+                            <i class="el-icon-location"></i>商品分类
+                        </el-menu-item>
+                </el-submenu>
+                <!-- 4 -->
+                <el-submenu index="4">
+                    <template slot="title">
+                        <i class="el-icon-location"></i>
+                        <span>订单管理</span>
+                    </template>
+            
+                        <el-menu-item index="1-1">
+                            <i class="el-icon-location"></i>订单列表
+                        </el-menu-item>
+                </el-submenu>
+                <!-- 5 -->
+                <el-submenu index="5">
+                    <template slot="title">
+                        <i class="el-icon-location"></i>
+                        <span>数据统计</span>
+                    </template>
+            
+                        <el-menu-item index="1-1">
+                            <i class="el-icon-location"></i>数据报表
+                        </el-menu-item>
+                </el-submenu>
+                
+            </el-menu>
+        </el-aside>
         <el-main class="main">Main</el-main>
     </el-container>
 </el-container>
@@ -29,7 +100,17 @@
 <script>
 export default {
     beforeCreate() {
-
+        const token=localStorage.getItem('token')
+        if(!token){
+            this.$router.push({name:'login'})
+        }
+    },
+    methods:{
+        loginout(){
+            this.$message.success('退出成功')
+            localStorage.clear()
+            this.$router.push({name:'login'})
+        }
     }
 }
 </script>
@@ -51,10 +132,12 @@ export default {
     background-color: #e9eef3;
     height: 100%;
 }
-.middle{
+
+.middle {
     text-align: center
 }
-.loginout{
+
+.loginout {
     text-align: center;
     line-height: 60px;
     text-decoration: none
