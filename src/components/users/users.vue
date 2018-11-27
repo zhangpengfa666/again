de19d0a6cf92ca87ba901e0ca0b9b110b8733f11
Zpf <347820927@qq.com>
2018-11-27 16:18:42 +0800

    'user面包屑导航及搜索框'

--- src/components/users/users.vue
+++ src/components/users/users.vue
@@ -1,13 +1,45 @@
 <template>
-    <div>6666666666666666666666666666666666666</div>
+<el-card class="box-card">
+    <!-- 1.面包屑 -->
+    <el-breadcrumb separator-class="el-icon-arrow-right">
+        <el-breadcrumb-item>首页</el-breadcrumb-item>
+        <el-breadcrumb-item>用户管理</el-breadcrumb-item>
+        <el-breadcrumb-item>用户列表</el-breadcrumb-item>
+    </el-breadcrumb>
+    <!-- 2.搜索框 -->
+    <el-row class="searchRow">
+        <el-col :span="16">
+            <el-input placeholder="请输入用户名" v-model="query" class="searchInput">
+                <el-button slot="append" icon="el-icon-search"></el-button>
+            </el-input>
+            <el-button type="primary">添加用户</el-button>
+        </el-col>
+    </el-row>
+    <!-- 3.表格 -->
+
+    <!-- 4.分页 -->
+
+</el-card>
 </template>
 
 <script>
 export default {
-
+    data () {
+        return {
+            query:''
+        }
+    }
 }
 </script>
 
 <style>
-
+.box-card {
+    height: 100%;
+}
+.searchRow{
+    margin-top: 20px;
+}
+.searchInput{
+    width: 300px;
+}
 </style>
